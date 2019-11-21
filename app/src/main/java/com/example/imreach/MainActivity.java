87b6723969f8c118a08ca6c01c523f79d3fdf641
Alissa <lisa32647@gmail.com>
2019-11-21 13:12:21 +0600

    Initial commit

---
+++ app/src/main/java/com/example/imreach/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.imreach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
